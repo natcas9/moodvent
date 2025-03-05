cb37344a5f800367657244c47e49d499e39ecd8c
Natalia Casarin <natalia.casarin02@gmail.com>
2025-03-05 22:13:50 +0100

    Cambio en nombre de file visualizarEventos

--- src/router.js
+++ src/router.js
@@ -6,10 +6,11 @@ const eventosRouter = express.Router();
 
 //Rutas para administar eventos
 eventosRouter.post("/eventos/crear", crearEvento);
+eventosRouter.post("eventos/crear", viewCrearEvento);
 eventosRouter.post("/eventos/modificar/:id", modificarEvento);
 eventosRouter.post("/eventos/borrar/:id", borrarEvento);
 
-usuariosRouter.get('/login', viewLogin);
-usuariosRouter.post('/login', doLogin);
-usuariosRouter.get('/logout', doLogout);
+usuariosRouter.get("/login", viewLogin);
+usuariosRouter.post("/login", doLogin);
+usuariosRouter.get("/logout", doLogout);
 export default eventosRouter;
