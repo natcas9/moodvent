1656fb64bc548f2d93e4b061f4ba0ce4cc58940c
Natalia Casarin <natalia.casarin02@gmail.com>
2025-03-06 11:12:17 +0100

    se agrego la plantilla del ejercicio dos

--- src/usuarios/controllers.js
+++ src/usuarios/controllers.js
@@ -123,6 +123,7 @@ export function doLogout(req, res, next) {
   req.session.save((err) => {
       if (err) next(err);
 
+<<<<<<< Updated upstream:src/controllers.js
       // regenerate the session, which is good practice to help
       // guard against forms of session fixation
       req.session.regenerate((err) => {
@@ -131,3 +132,76 @@ export function doLogout(req, res, next) {
       })
   })
 }
+=======
+export function viewEventos(req, res) {
+  res.render("paginas"),
+    {
+      contenido: "paginas/visualizarEventos",
+      eventos,
+    };
+}
+
+/*
+import { body } from 'express-validator';
+import { Usuario, RolesEnum } from './Usuario.js';
+
+export function viewLogin(req, res) {
+    let contenido = 'paginas/login';
+    if (req.session != null && req.session.login) {
+        contenido = 'paginas/home'
+    }
+    res.render('pagina', {
+        contenido,
+        session: req.session
+    });
+}
+
+export function doLogin(req, res) {
+    body('username').escape();
+    body('password').escape();
+    // Capturo las variables username y password
+    const username = req.body.username.trim();
+    const password = req.body.password.trim();
+
+    try {
+        const usuario = Usuario.login(username, password);
+        req.session.login = true;
+        req.session.nombre = usuario.nombre;
+        req.session.esAdmin = usuario.rol === RolesEnum.ADMIN;
+
+        return res.render('pagina', {
+            contenido: 'paginas/home',
+            session: req.session
+        });
+
+    } catch (e) {
+        res.render('pagina', {
+            contenido: 'paginas/login',
+            error: 'El usuario o contraseña no son válidos'
+        })
+    }
+}
+
+export function doLogout(req, res, next) {
+    // https://expressjs.com/en/resources/middleware/session.html
+    // logout logic
+
+    // clear the user from the session object and save.
+    // this will ensure that re-using the old session id
+    // does not have a logged in user
+    req.session.login = null
+    req.session.nombre = null;
+    req.session.esAdmin = null;
+    req.session.save((err) => {
+        if (err) next(err);
+
+        // regenerate the session, which is good practice to help
+        // guard against forms of session fixation
+        req.session.regenerate((err) => {
+            if (err) next(err)
+            res.redirect('/');
+        })
+    })
+}
+    */
+>>>>>>> Stashed changes:src/usuarios/controllers.js
--- src/usuarios/router.js
+++ src/usuarios/router.js
@@ -14,3 +14,15 @@ usuariosRouter.get("/login", viewLogin);
 usuariosRouter.post("/login", doLogin);
 usuariosRouter.get("/logout", doLogout);
 export default eventosRouter;
+
+/*import express from 'express';
+import { viewLogin, doLogin, doLogout } from './controllers.js';
+
+const usuariosRouter = express.Router();
+
+usuariosRouter.get('/login', viewLogin);
+usuariosRouter.post('/login', doLogin);
+usuariosRouter.get('/logout', doLogout);
+
+export default usuariosRouter;
+*/
