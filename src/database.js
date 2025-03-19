500715f90d05ac7e6ed9575204709242c4f18101
natcas9 <natalia.casarin02@gmail.com>
2025-03-19 22:57:20 +0100

    adding registration

--- src/database.js
+++ src/database.js
@@ -1,3 +1,4 @@
+// Se creó la base de datos con ayuda del ejercicio 2, chatgpt y el siguiente video https://www.youtube.com/watch?v=ehppT_7lYH0&ab_channel=LeonardoJose
 import { fileURLToPath } from "url";
 import { mkdirSync, existsSync } from "fs";
 import { join, dirname } from "node:path";
@@ -34,6 +35,19 @@ export function initDB() {
             precio REAL NOT NULL,
             estadoAnimo TEXT NOT NULL
         )
+    `);
+
+  db.exec(`
+      CREATE TABLE IF NOT EXISTS usuarios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        apellido TEXT NOT NULL,
+        edad INTEGER NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role TEXT CHECK(role IN ('admin', 'user')) NOT NULL
+      )
     `);
   //console.log("Base de datos inicializada correctamente");
 }
