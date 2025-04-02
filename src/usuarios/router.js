6b03d5665bd5490873b5e727437582828735a40c
natcas9 <natalia.casarin02@gmail.com>
2025-04-02 22:02:47 +0200

    ignorar logs y errores centralizados

--- src/usuarios/router.js
+++ src/usuarios/router.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { body } from "express-validator";
+import asyncHandler from "express-async-handler";
 
 import {
   viewLogin,
@@ -11,32 +12,9 @@ import {
 
 const usuariosRouter = express.Router();
 
-// Mostrar formulario de registro
-usuariosRouter.get("/registro", viewRegistro);
-
-// Procesar registro con validaciones
-usuariosRouter.post(
-  "/registro",
-  [
-    body("nombre", "El nombre es obligatorio").trim().notEmpty(),
-    body("apellido", "El apellido es obligatorio").trim().notEmpty(),
-    body("edad", "Edad obligatoria y debe ser un número válido").isInt({
-      min: 1,
-    }),
-    body("email", "Debes introducir un correo válido").isEmail(),
-    body("username", "El nombre de usuario es obligatorio").trim().notEmpty(),
-    body("password", "La contraseña debe tener al menos 6 caracteres")
-      .trim()
-      .isLength({ min: 6 }),
-    body("role", "El rol es obligatorio").notEmpty(),
-  ],
-  doRegistro
-);
-
-// Mostrar login
-usuariosRouter.get("/login", viewLogin);
-
-// Procesar login con validaciones
+usuariosRouter.get("/registro", asyncHandler(viewRegistro));
+usuariosRouter.post("/registro", asyncHandler(doRegistro));
+usuariosRouter.get("/login", asyncHandler(viewLogin));
 usuariosRouter.post(
   "/login",
   [
@@ -46,13 +24,10 @@ usuariosRouter.post(
       .withMessage("El nombre de usuario es obligatorio"),
     body("password").notEmpty().withMessage("La contraseña es obligatoria"),
   ],
-  doLogin
+  asyncHandler(doLogin)
 );
+usuariosRouter.get("/logout", asyncHandler(doLogout));
 
-// Logout
-usuariosRouter.get("/logout", doLogout);
-
-// Página para usuarios normales
 usuariosRouter.get("/normal", (req, res) => {
   res.render("pagina", {
     contenido: "paginas/normal",
