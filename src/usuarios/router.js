c55a68c0b145ca7207016be59a5ada9be5a5c083
natcas9 <natalia.casarin02@gmail.com>
2025-04-02 18:27:48 +0200

    added advance configuration

--- src/usuarios/router.js
+++ src/usuarios/router.js
@@ -11,11 +11,32 @@ import {
 
 const usuariosRouter = express.Router();
 
+// Mostrar formulario de registro
 usuariosRouter.get("/registro", viewRegistro);
 
-usuariosRouter.post("/registro", doRegistro);
+// Procesar registro con validaciones
+usuariosRouter.post(
+  "/registro",
+  [
+    body("nombre", "El nombre es obligatorio").trim().notEmpty(),
+    body("apellido", "El apellido es obligatorio").trim().notEmpty(),
+    body("edad", "Edad obligatoria y debe ser un número válido").isInt({
+      min: 1,
+    }),
+    body("email", "Debes introducir un correo válido").isEmail(),
+    body("username", "El nombre de usuario es obligatorio").trim().notEmpty(),
+    body("password", "La contraseña debe tener al menos 6 caracteres")
+      .trim()
+      .isLength({ min: 6 }),
+    body("role", "El rol es obligatorio").notEmpty(),
+  ],
+  doRegistro
+);
 
+// Mostrar login
 usuariosRouter.get("/login", viewLogin);
+
+// Procesar login con validaciones
 usuariosRouter.post(
   "/login",
   [
@@ -27,8 +48,11 @@ usuariosRouter.post(
   ],
   doLogin
 );
+
+// Logout
 usuariosRouter.get("/logout", doLogout);
 
+// Página para usuarios normales
 usuariosRouter.get("/normal", (req, res) => {
   res.render("pagina", {
     contenido: "paginas/normal",
