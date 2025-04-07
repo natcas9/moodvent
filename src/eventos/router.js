b1734b005d4099468da091c884bdf96d2809158b
natcas9 <natalia.casarin02@gmail.com>
2025-04-07 23:09:45 +0200

    filtrar eventos

--- src/eventos/router.js
+++ src/eventos/router.js
@@ -15,7 +15,7 @@ const router = express.Router();
 
 router.get("/crearEventos", asyncHandler(viewCrearEvento));
 router.post("/crear", asyncHandler(crearEvento));
-router.get("/visualizar", asyncHandler(viewEventos));
+router.get("/visualizarEventos", asyncHandler(viewEventos));
 router.get("/editar/:id", asyncHandler(viewEditarEvento));
 router.post("/modificar/:id", asyncHandler(modificarEvento));
 router.post("/eliminar/:id", asyncHandler(eliminarEvento));
