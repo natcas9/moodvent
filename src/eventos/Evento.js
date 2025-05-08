8a4c528c1d4d47de33c4c3eeb20648df86cc6fc9
Sofi Guerrero <sofiguerrerom3009@gmail.com>
2025-05-08 10:41:23 +0200

    nuevo commit

--- src/eventos/Evento.js
+++ src/eventos/Evento.js
@@ -201,6 +201,10 @@ export class Evento {
       return;
     }
 
+    const hoy = new Date().toISOString.split("T")[0];
+    const testHoy = this.db.prepare(`
+      SELECT * FROM TestResults 
+      WHERE username = ? AND DATE(fecha) = ?`)
     const stmt = this.db.prepare(`
       INSERT INTO TestResults (username, mood, fecha)
       VALUES(?,?,datetime('now'))
